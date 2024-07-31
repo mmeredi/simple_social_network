d702d05954fc62cc8d11e2d0457418fecb7ab109
mmeredi <mmeredi1@terpmail.umd.edu>
2024-07-31 15:42:59 -0400

    added goodbye function to simple_social_network.py

--- simple_social_network.py
+++ simple_social_network.py
@@ -1,3 +1,7 @@
 def introduction():
     """ A simple piece of code that introduces the user and their task"""
     print("Hello! I'm Michael Meredith and I'm working on exercise 9!")
+
+def goodbye():
+    """A simple function that prints a string to say 'goodbye' """
+    print("goodbye")
